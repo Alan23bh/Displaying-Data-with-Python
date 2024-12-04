b99b9c078d526e24e43af643fc0b6e990a0671b7
Alan Hernandez <alan23bh@gmail.com>
2024-12-03 21:23:23 -0500

    Made a histogram

--- trending-temperatures/main.py
+++ trending-temperatures/main.py
@@ -4,8 +4,9 @@ month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
 temp = [28,32,31,40,45,55,60,65,54,43,34,30]
 
 plt.plot(month,temp,color="purple")
-plt.xlabel("Month", fontsize="16")
-plt.ylabel("Temp in Farenheit", fontsize="16")
+plt.xlabel("Month", fontsize=16)
+plt.ylabel("Temp in Farenheit", fontsize=16)
 plt.title("Avg Temperatures for 2018 in North Pole, Alaska")
+
 
 plt.savefig("north_pole_temps.png")
